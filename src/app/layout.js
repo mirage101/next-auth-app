a28839a72897dedd8c2827872abe7c993582809c
ivan <ivan@stanga.net>
2024-08-07 13:48:57 +0300

    Add authentication using clerk

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
+import { ClerkProvider, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+import Loader from "@/components/Loader";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,8 +13,18 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <ClerkLoading>
+        <Loader/>
+      </ClerkLoading>
+      <ClerkLoaded>
+      <Header/>
+      {children}
+      </ClerkLoaded>      
+      </body>
     </html>
+    </ClerkProvider>
   );
 }
